8367ce669b365121a6e6afaa112e506321cbf29e
Saafin <zhsaafinhossen@gmail.com>
2021-06-13 10:21:54 +0600

    1

---
+++ LAB_TASK_03_PHP_INTRO/1.php
@@ -0,0 +1,11 @@
+<?php
+
+    $length= 5;
+    $width=10;
+    $Rectangle=$length*$width;
+    $p=$length + $width;
+    $perimeter= 2*$p;
+    echo "Rectangle= $Rectangle <br>";
+    echo "perimeter=$perimeter"
+
+?>
