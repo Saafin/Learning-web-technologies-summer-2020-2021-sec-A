b61765927ae9fd34cbbbffb6717799ecc665a45d
Saafin <72722412+Saafin@users.noreply.github.com>
2021-07-06 11:11:15 +0600

    Delete
    Delete

---
+++ LAB_TASK_6_DELETE/Controller/delete.php
@@ -0,0 +1,15 @@
+<?php
+	$title = "Delete Page";
+	include('../view/header.php');
+	$id = $_GET['id'];
+	
+	
+				$status = deleteUser($id);
+
+				if($status){
+					echo "Done";
+				}else{
+					echo "Db error";
+				}
+	
+?>
