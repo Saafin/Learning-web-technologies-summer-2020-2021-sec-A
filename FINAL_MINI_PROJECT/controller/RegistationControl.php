096ce85324998f9a53dc1d58a7639077a7ae9366
Saafin <72722412+Saafin@users.noreply.github.com>
2021-08-01 17:52:10 +0600

    Completed
    Completed

--- FINAL_MINI_PROJECT/controller/RegistationControl.php
+++ FINAL_MINI_PROJECT/controller/RegistationControl.php
@@ -1,7 +1,7 @@
 <?php
 	session_start();
     $idErr = $unameErr = $emailErr = $passwordErr = $cpasswordErr ="";
-    $id = $uname = $email = $password = $cpassword ="";
+    $id = $uname = $email = $password = $cpassword = $type="";
 	$flag=0;
 		if ($_SERVER["REQUEST_METHOD"] == "POST") {
 			if (empty($_POST["name"])) {
@@ -9,6 +9,12 @@
 			   $flag=1;
             }else {
                $uname = test_input($_POST["name"]);
+            }
+            if (empty($_POST["id"])) {
+               $idErr = "user ID is required";
+			   $flag=1;
+            }else {
+               $id = test_input($_POST["id"]);
             }
 			if (empty($_POST["email"])) {
                $emailErr = "email is required";
@@ -47,13 +53,13 @@ function test_input($data) {
 if($flag==1){
 echo $unameErr;
 echo $emailErr;
-echo $emailErr;
-echo $genderErr;
 echo $cpasswordErr;
 echo $passwordErr;
 header("refresh: 2; url = ../view/Registration.php");
 }
 else{
+    require_once '../model/userModel.php';
+    reg($id,$uname,$password,$email,$type);
     header("refresh: 2; url = ../view/login.php");
 }
 
--- FINAL_MINI_PROJECT/controller/loginControl.php
+++ FINAL_MINI_PROJECT/controller/loginControl.php
@@ -1,24 +1,20 @@
 <?php
 	session_start();
-	if(isset($_POST['submit'])){
+	if($_SERVER["REQUEST_METHOD"] == "POST"){
 		$id = $_POST['id'];
 		$password = $_POST['password'];
 		if($id != '' && $password != ''){
-			if($id=="0"&&$password="0"){
 					$_SESSION['flag'] = 'true';
+                    $_SESSION["id"]="$id";
 					setcookie('flag','true',time()+300,'/');
-					if($reg[0]=$username && $reg[1]=$password){
-						echo "done";
-                    }
-					header('location: ../view/home.php');
-			}
-			else{
-				echo 'invlaid username/password';
-			}
+					include '../model/userModel.php';
+                    login($id,$password);
 		}else{
 			echo "null value found...";
+            header('refresh: 2; url = ../view/login.php');
 		}
 	}else{
 		echo "invalid request...";
+        header('refresh: 2; url = ../view/login.php');
 	}
 ?>
